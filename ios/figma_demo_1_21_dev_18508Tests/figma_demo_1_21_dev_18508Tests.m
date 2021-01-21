42825ea7faca12a3da3e239ca00c7dabbb7de8b4
Crowdbotics <team@crowdbotics.com>
2021-01-21 19:51:09 +0000

    Initial commit.

---
+++ ios/figma_demo_1_21_dev_18508Tests/figma_demo_1_21_dev_18508Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface figma_demo_1_21_dev_18508 : XCTestCase
+
+@end
+
+@implementation figma_demo_1_21_dev_18508
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
